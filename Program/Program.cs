9244cbdb5ea5a454f8b7c5dc574f6402987b98fd
surkov-ia <Frost220v@gmail.com>
2023-06-29 12:46:47 +0500

    added method to sort array by length

--- Program/Program.cs
+++ Program/Program.cs
@@ -28,3 +28,27 @@ void PrintArray1(string[] array1)
     Console.WriteLine();
 }
 PrintArray1(array1);
+
+void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+SortArray(array1, array2);
+
+void PrintArray2(string[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray2(array2);
